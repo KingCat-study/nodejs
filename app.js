4917cd2bc65d52da660d93f94ab09b9f501be7ce
KingCat-study <jihunrock@gmail.com>
2020-08-14 21:41:06 +0900

    템플릿 pug

--- app.js
+++ app.js
@@ -1,11 +1,47 @@
 const express = require('express');
 const app = express();
+app.locals.pretty = true;
 
 //절대 경로가 아닌 상대 경로로 변경하는 방법이 없을까?
 app.use(express.static('D:/workspace/git/nodejs/public'));
 
+app.set('view engine', 'pug');
+
+//'./views' <-- 폴더명이 되어야함'
+app.set('views', './views');
+
+app.get('/template' , (req, res) => {
+  //'temp' == 'temp.pug'
+  res.render('temp2');
+});
+
 app.get('/', function(req, res) {
   res.send("Welcome To Home  어서오세요. 아픈 강소연");
+});
+
+app.get('/dynamic' , (req, res) =>{
+  var li_tags = '';
+  for (var i = 0; i < 5; i++) {
+    li_tags = li_tags + '<li>코딩 삽입 ' + i + '</li>';
+  }
+  var time = Date();
+
+  var output = `<!DOCTYPE html>
+  <html" dir="ltr">
+    <head>
+      <meta charset="utf-8">
+      <title></title>
+    </head>
+    <body>
+      이 페이지는 동적인 페이지 입니다.!!!
+      <ul>
+      ${li_tags}
+      </ul>
+      ${time}
+    </body>
+  </html>`;
+
+  res.send(output);
 });
 
 app.get('/cat' , (req, res) => {
